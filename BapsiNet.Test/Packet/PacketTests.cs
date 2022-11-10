2be595a1c32ce15c17aa2a06d9eb415e19839882
Christian I. Nilsson <nikize@gmail.com>
2022-11-10 14:44:58 +0100

    Add BapsiPacket class and tests

---
+++ BapsiNet.Test/Packet/PacketTests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using BapsiNet.Packet;
+
+namespace BapsiNet.Test.Packet;
+
+[TestFixture]
+public class PacketTests
+{
+    public static readonly byte[] DefaultKey = {
+        0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99,
+        0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99};
+
+    public static BapsiPacket DecodeTest(ReadOnlyMemory<byte> bytes)
+    {
+        var p = new BapsiPacket(bytes);
+        Console.WriteLine(p.ToString());
+        Assert.That(p.Length, Is.EqualTo(bytes.Length - 1 - 1 - 1 - 1 - 4 - 2 - 16 - 1 - 1));
+
+        var x = p.Cipher(DefaultKey);
+        Console.Out.WriteHexLine(x);
+        Console.WriteLine(p.ToString());
+        Assert.That(x.Length, Is.EqualTo(p.Length + 1 + 1));
+
+        return p;
+    }
+
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 63 48 50 4e  00 02 54 de 1c ef 78 93  1a 92 bc 41 de 61 33 a4  ac 93 80 53 6c b5")]
+    [TestCase(DataClassType.SystemLogin, "01 01 01 01 2f 33 42 9f  00 09 14 ab 18 81 09 2d  ea 43 90 19 3a 87 4b 24  b2 4c 69 67 c5 5b 60 c6 0e d3 16 bc 66")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 0d 99 a5 fc  00 02 ca 50 90 9c ff bf  92 77 e6 6d 09 e1 4b cf  ae 6b 01 80 df af")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 b5 eb 74 7f  00 02 2a 22 01 1c 33 66  50 54 2e d0 a0 fc 2b aa  c2 0c f8 ea ff 60")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 a2 44 ae 41  00 02 8d 7d dd e9 2e 3c  2c c6 20 73 29 e2 16 68  10 90 38 30 eb eb")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 5a 9f b4 2b  00 02 14 6f 9f 70 5b 61  12 fd e1 b7 ae 8c fb 1d  b5 f3 4f b7 37 1b")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 f5 48 f2 09  00 02 7d 3e 48 08 62 10  e6 db c1 1d af f5 53 0e  34 6c 59 2c e6 96")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 dc 90 c1 e0  00 02 2f d6 76 87 24 46  8c 06 86 38 e6 bd be c7  98 eb e1 6a 5f 92")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 d1 c5 8c 82  00 02 5c 28 7f 27 3d 29  34 83 2f 9f 76 5b e6 4d  b1 48 d9 4a ab 19")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 30 61 32 5f  00 02 2d 56 e0 3b ab 50  09 99 69 5a f3 2f 7d 93  8d 22 9c cd 31 43")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 74 82 ac 9e  00 02 6d c2 36 2d 30 0e  bf cc 39 6a e6 57 0a 15  b7 5e 18 73 16 30")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 fc 9a f0 6d  00 02 45 27 1b e1 f7 9a  59 da 1c 0d 84 9f 14 59  fc f9 c6 8b 22 f5")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 10 68 15 95  00 02 78 90 57 db 07 74  83 cf 56 35 ba 6c 93 c4  2e bc d0 a8 28 b9")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 1f 27 b7 51  00 02 16 bb ac f0 45 9a  fe d5 7d 70 c3 71 cd 9c  f4 a5 1e 97 88 96")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 4a 06 9d 60  00 02 b9 37 49 6f 8a 6f  68 0c 02 6d 19 49 bf 34  93 99 58 1c d4 8e")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 27 d7 99 59  00 02 51 0a fb 6f c2 35  7c 19 a5 4f c2 c6 86 ff  c1 82 25 86 63 c8")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 be 0b b0 3f  00 02 60 ab 5c f2 43 78  3f 28 9d 22 45 ee 21 16  16 fb a3 ee f2 09")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 d8 dd 7b 55  00 02 73 3e f6 0b 96 31  90 54 31 aa 86 25 8e be  62 d0 4f f6 07 01")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 7a ce cd 33  00 02 da 4c 5a b6 12 29  91 9e 99 b3 95 68 54 79  75 bc b1 ee 39 66")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 ac 52 97 1a  00 02 e8 75 72 b1 fe 36  69 30 5a f4 11 06 f1 37  62 fe 25 79 f7 eb")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 7e c9 08 85  00 02 ae 7e bd 0e 69 89  95 10 d5 b7 e5 52 20 6f  ba e2 67 86 2d 26")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 4d b0 f8 00  00 02 ef 1e c3 67 9e 4e  80 73 b6 84 f3 98 38 b2  7d cf 9d 4c ec 3a")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 43 19 86 3d  00 02 c4 d1 94 63 b7 10  74 8a f6 d6 9f 8d 2a 4c  bc 52 0b f9 f4 bd")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 1d 58 00 65  00 02 86 35 2f 4c f6 e3  cc 4b 95 8e e1 6e 8c f8  a9 87 22 50 93 9d")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 31 00 04 ac  00 02 05 68 b6 a6 97 6c  fe 31 eb 59 97 d2 17 f9  f7 dc e8 4b b0 11")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 af 0e e5 28  00 02 2e 7b e0 00 14 10  80 a3 f2 80 de a5 78 0e  5f e7 ea 36 e7 bd")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 27 63 50 e3  00 02 66 e7 e9 9a 11 b2  62 c2 ab 30 cd 24 7c 37  57 9d 10 c1 fd 30")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 4b 78 30 2f  00 02 d5 77 2b 6c dc 80  99 b3 65 6e f0 57 53 46  5e 9f 81 82 b3 79")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 f7 4d ce 39  00 02 26 31 bf c6 4a a2  b8 e1 c9 6a c5 49 a1 47  96 32 56 f1 3f 9b")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 6e a2 3d e1  00 02 3f 54 b9 a0 fc 4a  f4 53 54 5c 8a 19 c8 bb  ad 0c 3c 93 c5 08")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 e7 6a f5 c9  00 02 e9 51 92 d8 e0 b1  52 28 4c 36 c1 3e c2 e6  74 14 29 b3 c9 2f")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 48 7d be ac  00 02 68 39 7c de 6d f3  56 fd fa 9e 87 8e 62 9b  1c 76 8e d4 33 6d")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 31 8d d0 f2  00 02 9d 44 8c 0f 52 77  bc b1 9b dd 40 44 7b 39  0d ae b4 90 b8 02")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 3c 5b 38 81  00 02 e5 3e 84 8b 69 c4  b9 98 e9 52 34 8b b6 8e  22 29 2c 63 c6 c3")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 83 2a 7d d5  00 02 1e 87 ea e6 7d a4  12 1d 1d ae b2 33 e0 85  b4 03 21 37 d4 8f")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 63 74 84 53  00 02 2d 05 7b c0 dc 1f  3a 84 eb 12 6b c8 fb 22  23 29 35 78 87 cf")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 80 dd b1 da  00 02 19 eb 10 1e de ba  e3 75 e5 f5 45 7b 37 dd  9d b2 70 38 9d 2a")]
+    [TestCase(DataClassType.SystemPing, "01 01 01 01 eb fa 25 e7  00 02 ec b1 04 53 89 af  40 06 8d ce bd 0a 6b 42  d3 f7 9d 9c 84 b5")]
+    [TestCase(DataClassType.SingleResponseAck, "01 01 01 01 82 e8 0b 93  00 04 f8 12 c5 97 11 ef b7  a9 49 47 4f ff 2f 2b  65 d5 29 d5 a2 a2 46 46")]
+    [TestCase(DataClassType.SingleResponseAck, "01 01 01 01 ab f7 eb 2d  00 04 f2 9c b8 6b e8 9f  bb 6e 48 41 77 63 ec 04  75 ec da d8 47 2b e1 66")]
+    [TestCase(DataClassType.SystemLogin, "01 01 01 01 73 0b a8 fc  00 09 ce f6 10 bc 62 62  50 be 80 b5 61 18 eb 40  13 19 cd b4 85 d5 d9 9f  f5 13 ca b8 27")]
+    [TestCase(DataClassType.SingleResponseAck, "01 01 01 01 69 64 2f fd  00 04 96 c6 13 28 0c f8  37 e3 15 d9 6c d7 21 8b  91 ff 3d 82 31 d6 db c5")]
+    [TestCase(DataClassType.SingleResponseAck, "01 01 01 01 0c 20 4c ed  00 04 fe c3 1e c3 51 4c  03 c7 22 18 97 e9 44 77  1c c9 a0 ea 81 0a 25 fd")]
+    [TestCase(DataClassType.SingleResponseAck, "01 01 01 01 ac 66 eb 74  00 04 96 df a2 2c e3 db  d9 ae ce a5 b5 e4 0e c3  9a a0 54 3a 9f 38 60 a6")]
+    [TestCase(DataClassType.SystemConnectionTerminated, "01 01 01 01 4e 18 d0 44  00 00 e6 dc 14 c8 9b 3b  d7 80 40 a2 f2 dd d8 44  93 26 0b bf")]
+    [TestCase(DataClassType.SingleResponseAck, "01 01 01 01 3e 23 29 e7  00 01 5b 6e 8f ba 71 b3  19 05 60 93 cc c6 30 9c  2b 5a 1e b7 a7")]
+    public void DecodeWithTypeTest(DataClassType expectedClassAndType, string data)
+    {
+        Memory<byte> bytes = Tools.HexStringToBytes(data).ToArray();
+        var p = DecodeTest(bytes);
+
+        Assert.That(p.DataClassAndType, Is.EqualTo(expectedClassAndType),
+            $"{p.DataClassAndType} 0x{(ushort)p.DataClassAndType:x4} vs expected {expectedClassAndType} 0x{(ushort)expectedClassAndType:x4}");
+    }
+
+    [TestCase("01 01 01 01 4e 18 d0 44  00 00 e6 dc 14 c8 9b 3b  d7 80 40 a2 f2 dd d8 44  93 26 0b bf")]
+    [TestCase("01 01 01 01 73 0b a8 fc  00 09 ce f6 10 bc 62 62  50 be 80 b5 61 18 eb 40  13 19 cd b4 85 d5 d9 9f  f5 13 ca b8 27")]
+    [TestCase("01 01 01 01 2f 33 42 9f  00 09 14 ab 18 81 09 2d  ea 43 90 19 3a 87 4b 24  b2 4c 69 67 c5 5b 60 c6  0e d3 16 bc 66")]
+    public void PacketRecreationTest(string data)
+    {
+        Memory<byte> bytes = Tools.HexStringToBytes(data).ToArray();
+        Console.Out.WriteHexLine(bytes.Span);
+        var p = DecodeTest(bytes);
+
+        var newP = new BapsiPacket(p.DataClassAndType, p.DataItems.Value.Span);
+        Console.WriteLine(newP.ToString());
+        Assert.That(newP.Start0, Is.EqualTo(p.Start0), nameof(p.Start0));
+        Assert.That(newP.Start1, Is.EqualTo(p.Start1), nameof(p.Start1));
+        Assert.That(newP.Version, Is.EqualTo(p.Version), nameof(p.Version));
+        Assert.That(newP.Encryption, Is.EqualTo(EncryptionType.Unencrypted), nameof(p.Encryption));
+        Assert.That(newP.Length, Is.EqualTo(p.Length), nameof(p.Length));
+        Assert.That(newP.DataClassAndType, Is.EqualTo(p.DataClassAndType), $"DataClassAndType {newP.DataClassAndType}");
+        Assert.That(newP.DataText, Is.EqualTo(p.DataText), nameof(p.DataText));
+
+        var p2 = DecodeTest(p.GetDecodedPacket(DefaultKey).GetCryptPacket(DefaultKey));
+        Assert.That(p2.ToString(), Is.EqualTo(p.ToString()));
+
+        var packetBytes = newP.GetCryptPacket(DefaultKey);
+        Console.Out.WriteHexLine(packetBytes.Span);
+        var restoreP = DecodeTest(packetBytes);
+        Assert.That(restoreP.ToString(), Is.EqualTo(p.ToString()));
+    }
+}
---
+++ BapsiNet/Packet/BapsiPacket.cs
@@ -0,0 +1,222 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BapsiNet.Packet;
+
+public class BapsiPacket
+{
+    /// <summary>Raw on wire packet</summary>
+    private readonly ReadOnlyMemory<byte> _packet;
+
+    /// <summary>P:0 Start <SOH> 1B Start bit-pattern (0x01) E:No H:No</summary>
+    public byte Start0 => _packet.Span[0];
+    /// <summary>P:1 Start <SOH> 1B Start bit-pattern (0x01) E:No H:No</summary>
+    public byte Start1 => _packet.Span[1];
+    /// <summary>P:2 Version 1B Implemented BAPSI-version E:No H:No</summary>
+    public int Version => _packet.Span[2];
+    /// <summary>P:3 Encryption 1B Type of encryption 0x00=Unencrypted, 0x01=RC4 type E:No H:No</summary>
+    /// <remarks>
+    /// The only message that may be unencrypted, <see cref="EncryptionType.Unencrypted"/>, is
+    /// Acknowledge (C: 0x61 T: 0x06) with response code 310, Server encryption error.
+    /// </remarks>
+    public EncryptionType Encryption => (EncryptionType)_packet.Span[3];
+    /// <summary>P:4 Crypt 4B Random crypt data E:No H:No</summary>
+    public ReadOnlyMemory<byte> Crypt => _packet.Slice(4, 4);
+    /// <summary>P:8 Length 2B Length of data (Short, big-endian) E:No H:No</summary>
+    public ushort Length => (ushort)((_packet.Span[8] << 8) + _packet.Span[9]);
+
+    /// <summary>P:10 MD5 Hash 16B MD5 hash of Data class, Data type and Data E:Yes H:No</summary>
+    public ReadOnlyMemory<byte> Md5 { get; private set; }
+
+    /// <summary>P:26 Data class 1B  E:Yes H:Yes</summary>
+    /// <remarks>Use <see cref="DataClassAndType"/> instead</remarks>
+    private DataClassType DataClass => _dataPart == null ?
+        DataClassType.Undefined :
+        (DataClassType)(_dataPart.Value[..1].Span[0] << 8);
+
+    /// <summary>P:27 Data type 1B  E:Yes H:Yes</summary>
+    /// <remarks>Use <see cref="DataClassAndType"/> instead</remarks>
+    private byte DataType => _dataPart == null ? (byte)0x00 : _dataPart.Value.Slice(1, 1).Span[0];
+
+    /// <summary>P:26-27 Data class 1B, Data type 1B  E:Yes H:Yes</summary>
+    public DataClassType DataClassAndType => _dataPart == null ? 
+        DataClassType.Undefined :
+        (DataClassType)((_dataPart.Value[..2].Span[0] << 8) | _dataPart.Value[..2].Span[1]);
+
+    /// <summary>P:28 Data items ..B TAB(ASCII-9) separated data NULL terminated E:Yes E:Yes</summary>
+    public ReadOnlyMemory<byte>? DataItems => _dataPart?[2..];
+    public string DataText => DataItems == null ? string.Empty : DataEncoding.GetString(DataItems.Value.Span);
+
+    private static readonly Encoding DataEncoding = Encoding.ASCII;
+
+    /// <summary>Pointer to encrypted data in <see cref="_packet"/></summary>
+    private ReadOnlyMemory<byte> EncryptedPart => _packet[10..];
+
+    /// <summary>Decrypted data (hashed part), including <see cref="DataClass"/> and <see cref="DataType"/> but excluding <see cref="Md5"/></summary>
+    private ReadOnlyMemory<byte>? _dataPart;
+
+    /// <summary>
+    /// Create packet structure from a set of bytes
+    /// </summary>
+    public BapsiPacket(ReadOnlyMemory<byte> packet)
+    {
+        _packet = packet;
+        if (Start0 != 0x01)
+            throw new ArgumentOutOfRangeException(nameof(packet), packet, "Non expected SOH0");
+        if (Start1 != 0x01)
+            throw new ArgumentOutOfRangeException(nameof(packet), packet, "Non expected SOH1");
+        if (Version != 1)
+            throw new ArgumentOutOfRangeException(nameof(packet), packet, "Non expected Version");
+    }
+
+    /// <summary>
+    /// Generate new data packet, similar to BAPSIDLL PutBuffer using string
+    /// </summary>
+    public BapsiPacket(DataClassType dataClassAndType, string data)
+        : this(dataClassAndType, DataEncoding.GetBytes(data))
+    {
+    }
+
+    /// <summary>
+    /// Generate new data packet, similar to BAPSIDLL PutBuffer using a set of bytes
+    /// </summary>
+    public BapsiPacket(DataClassType dataClassAndType, ReadOnlySpan<byte> data)
+    {
+        // prepare static packet parts
+        Memory<byte> packet = new byte[28 + data.Length];
+        _packet = packet;
+        var header = packet.Span;
+        header[0] = 0x01; // Start0
+        header[1] = 0x01; // Start1
+        header[2] = 1; // Version
+        header[3] = (byte)EncryptionType.Unencrypted;
+        // Crypt 4 bytes set by GetCryptPacket
+        // Length 2 bytes ...
+        var length = (ushort)data.Length;
+        header[8] = (byte)(length >> 8 & 0xff);
+        header[9] = (byte)(length & 0xff);
+        // MD5 calculated and set later
+
+        // allocate data
+        header[26] = (byte)((ushort)dataClassAndType >> 8 & 0xff);
+        header[27] = (byte)((ushort)dataClassAndType & 0xff);
+        data.CopyTo(header[28..]);
+        var dPartM = packet[26..];
+        _dataPart = dPartM;
+
+        MD5.HashData(dPartM.Span, packet.Slice(10, 16).Span);
+#if DEBUG // validate correct hash implemenation
+        VerifyHash(packet[10..]);
+#endif
+    }
+
+    /// <summary>Get encrypted packet, <see cref="Crypt"/> part of returned buffer gets a new random value</summary>
+    /// <exception cref="InvalidOperationException">If not <see cref="EncryptionType.Unencrypted"/></exception>
+    public ReadOnlyMemory<byte> GetCryptPacket(byte[] key)
+    {
+        if (Encryption != EncryptionType.Unencrypted)
+            throw new InvalidOperationException("Can only get packet from unencrypted instance");
+
+        // get a packet copy
+        Memory<byte> buffer = _packet.ToArray();
+        var header = buffer.Span;
+        header[3] = (byte)EncryptionType.RC4;
+        RandomNumberGenerator.Fill(header.Slice(4, 4));
+        ApplyCiper(header[10..], key, buffer.Slice(4, 4));
+
+#if DEBUG
+        // verify packet while developing
+        var bPacket = new BapsiPacket(buffer);
+        bPacket.Cipher(key);
+#endif
+
+        return buffer;
+    }
+
+    /// <summary>Do the actual RC4 ciper manipulation of <paramref name="buffer"/></summary>
+    /// <exception cref="ArgumentOutOfRangeException">If <paramref name="key"/> is not valid</exception>
+    private static void ApplyCiper(Span<byte> buffer, byte[] key, ReadOnlyMemory<byte> crypt)
+    {
+        if (key.Length != 16)
+            throw new ArgumentOutOfRangeException(nameof(key), key, "must be 16 bytes");
+
+        // combine key with crypt
+        var rc = new RC4(key.Concat(crypt.ToArray()).ToArray());
+        // apply ciper to data
+        rc.Apply(buffer);
+    }
+
+    /// <summary>Apply decryption <paramref name="key"/> to packet to get decoded data and verify result</summary>
+    /// <exception cref="InvalidOperationException">If MD5 dosn't match</exception>
+    public ReadOnlySpan<byte> Cipher(byte[] key)
+    {
+        // a copy that can be manipulated
+        Memory<byte> buffer = EncryptedPart.ToArray();
+        if (Encryption == EncryptionType.RC4)
+        {
+            ApplyCiper(buffer.Span, key, Crypt);
+        }
+
+        var data = VerifyHash(buffer);
+        _dataPart = data;
+        return data.Span;
+    }
+
+    /// <summary>Verify hash of the encrypted data</summary>
+    /// <exception cref="InvalidOperationException">If MD5 dosn't match</exception>
+    private Memory<byte> VerifyHash(Memory<byte> buffer)
+    {
+        // get data part (excluding MD5)
+        var data = buffer[16..];
+        var buf = buffer.Span;
+        // compute hash on data
+        var calcHash = GC.AllocateUninitializedArray<byte>(128 / 8);
+        MD5.HashData(data.Span, calcHash);
+        // verify hash
+        for (int i = 0; i < calcHash.Length; i++)
+        {
+            if (calcHash[i] != buf[i])
+                throw new InvalidOperationException($"Invalid hash {i}: {calcHash[i]:x2} vs {buf[i]:x2}");
+        }
+        // we are valid, save the extracted data
+        Md5 = calcHash;
+        return data;
+    }
+
+    /// <summary>
+    /// Get a <see cref="EncryptionType.Unencrypted"/> instance of packet,
+    /// (<see cref="DataClassAndType"/> and <see cref="DataItems"/> is copied)
+    /// </summary>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// If packet is <see cref="EncryptionType.RC4"/> and <paramref name="key"/> is not valid
+    /// </exception>
+    public BapsiPacket GetDecodedPacket(byte[] key)
+    {
+        if (DataItems == null)
+            Cipher(key);
+        var p = new BapsiPacket(DataClassAndType, DataItems.Value.Span);
+        return p;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"V:{Version} E:{Encryption} L:{Length}");
+        var dct = DataClassAndType;
+        if (DataItems != null && dct != DataClassType.Undefined)
+        {
+            if (!Enum.IsDefined(dct))
+                sb.Append($" C:{DataClass}");
+            sb.Append($" CT:{dct}(0x{(ushort)dct:x4}) Txt:")
+                .Append(DataText).Append(" D:");
+            foreach (var b in DataItems.Value.Span)
+            {
+                if (char.IsLetterOrDigit((char)b))
+                    sb.Append((char)b);
+                else sb.Append($" 0x{b:X2} ");
+            }
+        }
+        return sb.ToString();
+    }
+}

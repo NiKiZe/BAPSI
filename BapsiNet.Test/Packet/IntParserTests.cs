dafa2e0da7bf3f61136cf10e64e7429910f48dcd
Christian I. Nilsson <nikize@gmail.com>
2022-11-22 18:31:05 +0100

    BapsiCommand structure start
    Integer parsing is a common element

---
+++ BapsiNet.Test/Packet/IntParserTests.cs
@@ -0,0 +1,24 @@
+﻿
+using BapsiNet.Command;
+
+namespace BapsiNet.Test.Packet;
+
+[TestFixture]
+public class IntParserTests
+{
+    [TestCase(null, "")]
+    [TestCase(null, "64 00 00")]
+    [TestCase(null, "00 00")]
+    [TestCase(100, "31 30 30 00")]
+    [TestCase(400, "34 30 30")]
+    [TestCase(0, "30")]
+    [TestCase(4, "30 34")]
+    [TestCase(9, "39 00")]
+    [TestCase(null, "31 30 65 00")]
+    [TestCase(null, "31 30 30 00 31")]
+    public void BaseParseIntTest(int? expected, string data)
+    {
+        Memory<byte> bytes = Tools.HexStringToBytes(data).ToArray();
+        Assert.That(BapsiCommand.GetInteger(bytes.Span), Is.EqualTo(expected));
+    }
+}
---
+++ BapsiNet/Command/BapsiCommand.cs
@@ -0,0 +1,49 @@
+﻿
+using BapsiNet.Packet;
+
+namespace BapsiNet.Command;
+
+public abstract class BapsiCommand
+{
+    public abstract DataClassType DataClassAndType { get; }
+
+    internal abstract ReadOnlyMemory<byte> Data { get; }
+
+    public virtual BapsiPacket GetPacket() => new(DataClassAndType, Data.Span);
+
+    #region GetInteger
+    private static int GetCharInt(char c) => '0' <= c && c <= '9' ? c - '0' : -1;
+
+    public static int? GetInteger(ReadOnlySpan<byte> data)
+    {
+        int i = 0;
+        bool hasDigit = false;
+        bool hasNull = false;
+        foreach (var b in data)
+        {
+            if (b == 0)
+            {
+                if (!hasDigit)
+                    return null;
+                hasNull = true;
+                continue;
+            }
+
+            var cval = GetCharInt((char)b);
+            if (cval == -1)
+                return null;
+
+            // we got digit after null, so invalid
+            if (hasNull)
+                return null;
+
+            i *= 10;
+            i += cval;
+            hasDigit = true;
+        }
+        if (!hasDigit)
+            return null;
+        return i;
+    }
+    #endregion
+}

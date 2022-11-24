0e23daa92779a1a1335d34adabd89e8c6e52adda
Christian I. Nilsson <nikize@gmail.com>
2022-11-24 11:08:23 +0100

    Add SingleResponseServerResponseCommand

---
+++ BapsiNet/Command/SingleResponseServerResponseCommand.cs
@@ -0,0 +1,29 @@
+﻿
+using BapsiNet.Packet;
+
+namespace BapsiNet.Command;
+
+/// <summary>Implements <see cref="DataClassType.SingleResponseServerResponse"/></summary>
+public class SingleResponseServerResponseCommand : ParameterizedCommand
+{
+    public override DataClassType DataClassAndType => DataClassType.SingleResponseServerResponse;
+    public ServerTypeType ServerType => (ServerTypeType)Convert.ToInt32(Parameters[0]);
+    public string Version => Parameters[1];
+    public int Revision => Convert.ToInt32(Parameters[2]);
+
+    private SingleResponseServerResponseCommand() : base(Array.Empty<string>()) { }
+
+    public enum ServerTypeType
+    {
+        Entro = 1,
+    }
+
+    public SingleResponseServerResponseCommand(ServerTypeType serverType, string version, int revision)
+        : base(new string[] { ((int)serverType).ToString(), version, revision.ToString()}) {}
+
+    public SingleResponseServerResponseCommand(ReadOnlyMemory<byte> data)
+        : base(data) {}
+
+    public override string ToString() => $"SingleResponseServerResponseCommand : Type: {ServerType} Version: {Version} Revision: {Revision}";
+}
+

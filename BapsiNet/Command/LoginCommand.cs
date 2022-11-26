a311d8d686b6c6242f1754a5b0ccd63d9b82dd67
Christian I. Nilsson <nikize@gmail.com>
2022-11-26 20:08:49 +0100

    Add LoginCommand

---
+++ BapsiNet/Command/LoginCommand.cs
@@ -0,0 +1,27 @@
+﻿
+using BapsiNet.Packet;
+
+namespace BapsiNet.Command;
+
+/// <summary>Implements <see cref="DataClassType.SystemLogin"/></summary>
+public class LoginCommand : BapsiCommand
+{
+    public override DataClassType DataClassAndType => DataClassType.SystemLogin;
+    public string BapsiUser { get; private set; }
+    internal override ReadOnlyMemory<byte> Data => GetBytes(BapsiUser);
+
+    private LoginCommand() : this(string.Empty) { }
+
+    public LoginCommand(string bapsiUser)
+        : base()
+    {
+        BapsiUser = bapsiUser;
+    }
+
+    public LoginCommand(ReadOnlyMemory<byte> data)
+        : this(GetString(data))
+    {
+    }
+
+    public override string ToString() => $"LoginCommand BapsiUser: {BapsiUser}";
+}
